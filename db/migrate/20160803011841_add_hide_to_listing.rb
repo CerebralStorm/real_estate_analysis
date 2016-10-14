60bc91469a29668006cdc5116f06e5ea0bc9ffe3
Cody Tanner <ctanner@instructure.com>
2016-10-13 20:59:15 -0600

    fixes typo and git ignore

--- db/migrate/20160803011841_add_hide_to_listing.rb
+++ db/migrate/20160803011841_add_hide_to_listing.rb
@@ -2,6 +2,6 @@ class AddHideToListing < ActiveRecord::Migration[5.0]
   def change
     add_column :listings, :hide, :boolean, default: false
     add_column :listings, :zip_code_id, :integer, index: true
-    remove_column :listings :zip_code
+    remove_column :listings, :zip_code
   end
 end
